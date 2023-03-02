2386d68571bdc5c56ff6a2673adc11a7a9cf5858
Daniil Fridkin <daniilfridkin@gmail.com>
2023-03-02 22:59:36 +0300

    finish

--- examples/cat.asm
+++
@@ -1,8 +0,0 @@
-.data:
-    num tmp 0
-.text:
-
-loop: in tmp
-    out_char tmp
-	jmp loop
-end: hlt
--- translator_integration_test.py
+++ translator_integration_test.py
@@ -7,6 +7,7 @@ import os
 import tempfile
 import pytest
 import isa
+import machine
 import translator
 
 
@@ -15,11 +16,18 @@ def test_prob2(golden):
     with tempfile.TemporaryDirectory() as tmpdirname:
         source = os.path.join(tmpdirname, "source.asm")
         bin = os.path.join(tmpdirname, "bin.json")
+        inp = os.path.join(tmpdirname, "inp.txt")
 
-        with open(source, "w", encoding="utf-8") as file:
-            file.write(golden["source"])
+        with open(source, "w", encoding="utf-8") as s,  open(inp, "w", encoding="utf-8") as i:
+            s.write(golden["source"])
+            i.write(golden.get("input", ""))
 
         translator.main([source, bin])
         result, data_section = isa.read_code(bin)
         assert result == golden.out['code']
         assert data_section == golden.out['data']
+
+        if not golden.get("input", None):
+            assert machine.main([bin]) == golden.out['output']
+        else:
+            assert machine.main([bin, inp]) == golden.out['output']
