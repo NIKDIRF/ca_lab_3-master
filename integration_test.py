b0e524cae1504b02169f279784817b41ec67b12f
Daniil Fridkin <daniilfridkin@gmail.com>
2023-03-02 23:12:54 +0300

    fix linter

--- integration_test.py
+++ integration_test.py
@@ -15,19 +15,19 @@ import translator
 def test_prob2(golden):
     with tempfile.TemporaryDirectory() as tmpdirname:
         source = os.path.join(tmpdirname, "source.asm")
-        bin = os.path.join(tmpdirname, "bin.json")
+        binary = os.path.join(tmpdirname, "binary.json")
         inp = os.path.join(tmpdirname, "inp.txt")
 
-        with open(source, "w", encoding="utf-8") as s,  open(inp, "w", encoding="utf-8") as i:
-            s.write(golden["source"])
-            i.write(golden.get("input", ""))
+        with open(source, "w", encoding="utf-8") as src,  open(inp, "w", encoding="utf-8") as inp:
+            src.write(golden["source"])
+            inp.write(golden.get("input", ""))
 
-        translator.main([source, bin])
-        result, data_section = isa.read_code(bin)
+        translator.main([source, binary])
+        result, data_section = isa.read_code(binary)
         assert result == golden.out['code']
         assert data_section == golden.out['data']
 
         if not golden.get("input", None):
-            assert machine.main([bin]) == golden.out['output']
+            assert machine.main([binary]) == golden.out['output']
         else:
-            assert machine.main([bin, inp]) == golden.out['output']
+            assert machine.main([binary, inp]) == golden.out['output']
